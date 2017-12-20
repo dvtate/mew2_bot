65e90e01d58d349465bb67bea5678aae900935c8
Dustin Van Tate Testa <toast27@gmail.com>
2017-12-19 21:43:15 -0500

    noun -> verb

--- cards.js
+++ cards.js
@@ -76,7 +76,7 @@ module.exports.card_file_id_index = {
 };
 
 // return a shuffledDeck of cards
-module.exports.shuffledDeck = (deck) => {
+module.exports.shuffleDeck = (deck) => {
     for (let i = deck.length; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [deck[i], deck[j]] = [deck[j], deck[i]];
