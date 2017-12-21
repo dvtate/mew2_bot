d91882ece4b42acf55e68a237bb9b58e36d306a6
Dustin Van Tate Testa <toast27@gmail.com>
2017-12-20 23:05:37 -0500

    fix shuffle bug

--- cards.js
+++ cards.js
@@ -77,7 +77,7 @@ module.exports.card_file_id_index = {
 
 // return a shuffledDeck of cards
 module.exports.shuffleDeck = (deck) => {
-    for (let i = deck.length; i > 0; i--) {
+    for (let i = deck.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [deck[i], deck[j]] = [deck[j], deck[i]];
     }
